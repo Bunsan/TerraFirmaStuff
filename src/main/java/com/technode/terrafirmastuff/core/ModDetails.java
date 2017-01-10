1e54101dfbdfb405a534c21edca65ccb8687e9c9
Bunsan <bunsan@users.noreply.github.com>
2017-01-10 12:03:31 +0930

    Version Bump.

--- src/main/java/com/technode/terrafirmastuff/core/ModDetails.java
+++ src/main/java/com/technode/terrafirmastuff/core/ModDetails.java
@@ -6,8 +6,8 @@ public class ModDetails
     public static final String ModName = "TerraFirmaStuff";
 
     public static final int VersionMajor = 0;
-    public static final int VersionMinor = 1;
-    public static final int VersionRevision = 4;
+    public static final int VersionMinor = 2;
+    public static final int VersionRevision = 0;
 
     public static final String ModVersion = VersionMajor + "." + VersionMinor + "." + VersionRevision;
 
